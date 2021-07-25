0a6a8672cc832c22ff528cbbbed774c551127b24
Husain Jinia <husainjinia0201@gmail.com>
2021-07-25 18:48:46 +0530

    Added view more page for blogs

--- Ewaah/blog/urls.py
+++ Ewaah/blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('blogs',views.home, name='blog-home'),
-
+    path('blogs/<int:blog_id>', views.viewblog, name = 'ViewBlog')
 ]
--- Ewaah/blog/views.py
+++ Ewaah/blog/views.py
@@ -5,4 +5,12 @@ from .models import Blog
 # Create your views here.
 def home(request):
     blog = Blog.objects.all()
-    return render(request,'blog/home.html', {"blogs": blog}) 
+    return render(request,'blog/blogpage.html', {"blogs": blog}) 
+
+def viewblog(request,blog_id):
+    blogs = Blog.objects.get(id= blog_id)
+   
+    return render(request, 'blog/ViewBlog.html', 
+    {
+    "blogs": blogs
+    })
